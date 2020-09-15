39d97b8eea819bd816622c028cd0732bee21353a
小凡 <zhangxuanshuo@innotechx.com>
2020-09-15 14:28:47 +0800

    feat: 删除vue

--- src/components/Modal/index.vue
+++ src/components/Modal/index.vue
@@ -5,13 +5,75 @@
 <template>
   <ol class="">
     <li>
-      <h3>1. 使用双向绑定控制显示。</h3>
-
-      <p>父元素代码</p>
+      <h3>布尔值双向绑定</h3>
+      <h4>父元素代码</h4>
       <code>
         {{ `
-        <Modal v-model="show" />
+        <MyModal v-model="show" />
         ` }}
+      </code>
+
+      <h4>modal代码</h4>
+      <code>
+        <p>// template</p>
+        {{ `
+        <Modal v-model="visible" />
+        ` }}
+        <p>// script</p>
+        <p>
+          <!-- eslint-disable -->
+          computed: { 
+            visible: { 
+              get() { return this.value; },
+              set() {
+                this.$emit('input', false);
+              },
+            }
+          } `
+          <!-- eslint-enable -->
+        </p>
+      </code>
+
+      <h5>template</h5>
+      <code>
+        {{`
+        <template>
+          <Modal v-model="visible">
+            <template v-slot:footer>
+              <div></div>
+            </template>
+          </Modal>
+        </template>
+        <script>
+          export default {
+            name: 'my-modal',
+            props: {
+              // 双向绑定，也可使用唯一值
+              value: { type: Boolean, default: false },
+            },
+          };
+        </script>
+        `}}
+      </code>
+      <h5>script</h5>
+      <code>
+        {{
+          `
+          props: {
+            value: { type: Boolean, default: false },
+          },
+          computed: {
+            visible: {
+              get() {
+                return this.value;
+              },
+              set() {
+                this.$emit('input', false);
+              },
+            },
+          },
+        `
+        }}
       </code>
     </li>
   </ol>
