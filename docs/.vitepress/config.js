27bbc93df38a176e0c1196d6d03f6fbd1bdda7d7
小凡 <2313528633@qq.com>
2022-02-09 16:30:11 +0800

    feat: update catalog

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,11 +2,11 @@ module.exports = {
   title: 'Vue Concepts',
   themeConfig: {
     sidebar: [
-      { text: '基础概念', link: 'base-concept', },
-      { text: '进阶概念', link: 'enhance-concept'},
-      { text: 'vue-router', link: 'vue-router' },
+      { text: '基础概念', link: 'base-concept/', },
+      { text: '进阶概念', link: 'enhance-concept/'},
+      { text: 'vue-router', link: 'vue-router/' },
       { text: 'vuex', link: 'vuex/', },
-      { text: '项目搭建', link: 'build' },
+      { text: '项目搭建', link: 'build/' },
       { text: '开发模板' },
     ],
   }
